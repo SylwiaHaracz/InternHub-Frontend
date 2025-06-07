4898f3912ae801fd41735c065462b584b94acb6f
SylwiaHaracz <sylwia.haracz@o2.pl>
2025-06-07 12:34:50 +0200

    feature: initial setup

--- intern-hub/src/App.js
+++ intern-hub/src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-content">
+          <div className='nav'>
+            <div className='navLogo'>
+              <p>InternHub</p>
+            </div>
+            <div className='navContent'>
+              <p>Dashboard</p>
+              <p>Job Board</p>
+              <p>My applications</p>
+              <p>Courses</p>
+              <p>Messages</p>
+              <p>Explore Companies</p>
+              <p>Settings</p>
+              <p>Support Center</p>
+            </div>
+          </div>
+          <div className='content'>
+            <div className='searchBar'>
+
+            </div>
+            <div className='contentBoxes'>
+              <h1>Dashboard</h1>
+              <div className='firstRow'>
+                <div className='box'>
+
+                </div>
+                <div className='box'>
+                  
+                </div>
+                <div className='box'>
+                  
+                </div>
+              </div>
+              <div className='secondRow'>
+                <div className='box'>
+                  
+                </div>
+                <div className='box'>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
       </header>
     </div>
   );
